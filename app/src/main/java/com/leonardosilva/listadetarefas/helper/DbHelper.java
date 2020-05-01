3c90debf675226e472b112d7cca3e42a5ecece86
leonardo534 <leonardoredes534@gmail.com>
2020-04-30 23:47:16 -0300

    Criado tabela

--- app/src/main/java/com/leonardosilva/listadetarefas/helper/DbHelper.java
+++ app/src/main/java/com/leonardosilva/listadetarefas/helper/DbHelper.java
@@ -3,11 +3,13 @@ package com.leonardosilva.listadetarefas.helper;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 public class DbHelper extends SQLiteOpenHelper {
 
     public static int VERSION = 1;
     public static String NOME_DB = "DB_TAREFAS";
+    public static String TABELA_TAREFAS = "tarefas";
 
     public DbHelper(Context context){
         super(context, NOME_DB, null, VERSION);
@@ -15,6 +17,15 @@ public class DbHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+
+        String sql = "CREATE TABLE IF NOT EXISTS "+TABELA_TAREFAS+" (id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT NOT NULL);";
+
+        try{
+            db.execSQL(sql);
+            Log.i("INFO DB", "SUCESSO: ");
+        }catch(Exception e){
+            Log.i("INFO DB", "ERROR: "+e.getMessage());
+        }
 
     }
 
